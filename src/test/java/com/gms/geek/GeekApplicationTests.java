0dd681783e84980355bb77d8961ca7a5854618aa
15374 <1537414333@qq.com>
2021-10-31 21:08:53 +0800

    Initial commit

---
+++ src/test/java/com/gms/geek/GeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gms.geek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
